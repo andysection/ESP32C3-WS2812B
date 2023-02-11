10fc8bbd51135ea479d191b6f19643f2777c6d18
SectionOne <398148947@qq.com>
2023-02-11 17:59:36 +0800

    Color set & right left push Action add

--- Code/iOS/digitalTV/ViewController.m
+++ Code/iOS/digitalTV/ViewController.m
@@ -8,18 +8,26 @@
 #import "ViewController.h"
 #import <Masonry.h>
 
+int colum = 17;
+int row = 5;
+
 @interface ViewController ()
 
-@property (nonatomic, strong) NSDictionary *viewDic;
+@property (nonatomic, strong) NSDictionary<NSNumber *, UIView *> *viewDic;
 
 @property (nonatomic, strong) UIStackView *grid;
+
+@property (weak, nonatomic) IBOutlet UIButton *colorBtn;
+@property (nonatomic, strong) UIColor *currentColor;
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupGrid];
+    self.currentColor = [self randColor];
 }
 
 - (void)setupGrid {
@@ -27,8 +35,7 @@
     grid.axis = UILayoutConstraintAxisVertical;
     grid.distribution = UIStackViewDistributionFillEqually;
     grid.spacing = 2;
-    int colum = 17;
-    int row = 5;
+
     NSMutableDictionary *mDic = @{}.mutableCopy;
     for (int j = 0; j < row; j ++) {
         UIStackView *stack = [[UIStackView alloc] init];
@@ -59,6 +66,7 @@
     }];
     self.grid.layer.borderColor = UIColor.blackColor.CGColor;
     self.grid.layer.borderWidth = 2;
+    self.viewDic = mDic.copy;
 }
 
 - (UIColor *)randColor {
@@ -68,14 +76,58 @@
     return [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
 }
 
-
-
 - (void)tapAction:(UITapGestureRecognizer *)sender {
     NSLog(@"%d", sender.view.tag);
-    sender.view.backgroundColor = UIColor.orangeColor;
+    sender.view.backgroundColor = self.currentColor;
 }
 
+- (IBAction)allOffAction:(id)sender {
+    for (UIView *view in self.viewDic.allValues) {
+        view.backgroundColor = UIColor.darkGrayColor;
+    }
+}
 
+- (IBAction)allSetAction:(id)sender {
+    for (UIView *view in self.viewDic.allValues) {
+        view.backgroundColor = self.currentColor;
+    }
+}
+
+- (IBAction)currentColor:(UIButton *)sender {
+    sender.backgroundColor = [self randColor];
+    self.currentColor = sender.backgroundColor;
+}
+
+- (void)setCurrentColor:(UIColor *)currentColor {
+    _currentColor = currentColor;
+    self.colorBtn.backgroundColor = currentColor;
+}
+
+- (IBAction)leftPush:(UIButton *)sender {
+    for (int i = 0; i < colum; i++) {
+        for (int j = 0; j < row; j ++) {
+            int index = i + j * colum;
+            if (i == colum - 1) {
+                self.viewDic[@(index)].backgroundColor = UIColor.darkGrayColor;
+            } else {
+                self.viewDic[@(index)].backgroundColor = self.viewDic[@(index  + 1)].backgroundColor;
+            }
+        }
+    }
+}
+
+- (IBAction)rightPush:(UIButton *)sender {
+    for (int i = colum - 1; i > -1; i--) {
+        for (int j = 0; j < row; j ++) {
+            int index = i + j * colum;
+            if (i == 0) {
+                self.viewDic[@(index)].backgroundColor = UIColor.darkGrayColor;
+            } else {
+                self.viewDic[@(index)].backgroundColor = self.viewDic[@(index  - 1)].backgroundColor;
+            }
+        }
+    }
+}
 
 
 @end
