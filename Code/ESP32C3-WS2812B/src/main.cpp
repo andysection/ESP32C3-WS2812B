15124e8f059341424ea5906b97f42bdb54a908ae
SectionOne <398148947@qq.com>
2023-02-12 20:16:16 +0800

    demo处理

--- Code/ESP32C3-WS2812B/src/main.cpp
+++ Code/ESP32C3-WS2812B/src/main.cpp
@@ -21,17 +21,32 @@ class LEDmodel
         float alpha; 
 };
 
-#define LED_PIN 4
-#define NUM_LEDS 90
+#define LED_PIN 2
+#define NUM_LEDS 60
 CRGB leds[NUM_LEDS];
 
 void setup() {
+  Serial.begin(115200);
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   std::map<int, LEDmodel> maps;
 }
 
 void loop() {
-  leds[1] = CRGB::Brown;
-  leds[1].nscale8(64);
-  FastLED.setBrightness(64);// Set the brightness to 64/255
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].setRGB(random(255), random(255), random(255));
+    if (i > 8) {
+      leds[i-8].setRGB(random(255), random(255), random(255));
+    }
+    if (i > 16) {
+      leds[i-16].setRGB(random(255), random(255), random(255));
+    }
+    leds[i-1].setRGB(random(0), random(0), random(0));
+    FastLED.show();
+    delay(100);
+    Serial.println("run");
+  }
+
+  // leds[1] = CRGB::Brown;
+  // leds[1].nscale8(64);
+  // FastLED.setBrightness(64);// Set the brightness to 64/255
 }
